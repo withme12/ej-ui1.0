29a32219591a021ee0586388575e9030554dd3e5
zan233 <2519282298@qq.com>
2019-06-13 01:11:52 +0800

    Comment页面的主要功能

--- src/routes/CommentPage.js
+++ src/routes/CommentPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './CommentPage.css'
 import {Modal,Button,Table,message} from 'antd'
 import axios from  '../utils/axios'
-
+import CommentForm from './CommentForm'
 
 
 //组件类必须继承React.Component
@@ -12,21 +12,22 @@ class CommentPage extends React.Component{
         this.state={
             ids:[],
             list:[],
-            loading:false
+            loading:false,
+            visible:false,
+            comment:{}
         }
     }
     componentDidMount(){
         this.reloadData();
     }
-    //查询所有
-    reloadData(){
+    reloadData(){   //重载数据
         this.setState({loading:true});
         axios.get("/comment/findAll")
         .then((result)=>{
             this.setState({list:result.data})
         })
         .finally(()=>{
-            this.setState({loading:false})
+            this.setState({loading:false});
         })
     }
     //批量删除
@@ -46,7 +47,7 @@ class CommentPage extends React.Component{
     //单个删除
     handleDelete(id){
         Modal.confirm({
-            title:'确定要删除这条评价吗？',
+            title:'确定要删除这条记录吗？',
             content:'删除后数据将无法修复',
             onOk:()=>{
                 axios.get("/comment/deleteCommentById",{
@@ -61,22 +62,59 @@ class CommentPage extends React.Component{
             }
         });
     }
-
-
+    // 取消按钮的事件处理函数
+    handleCancel = () => {
+        this.setState({ visible: false });
+    };
+    // 确认按钮的事件处理函数
+    handleCreate = () => {
+        const form = this.formRef.props.form;
+        form.validateFields((err, values) => {
+        if (err) {
+            return;
+        }
+        // 表单校验完成后与后台通信进行保存
+        axios.post("/comment/insertOrUpdate",values)
+        .then((result)=>{
+            message.success(result.statusText)
+            // 重置表单
+            form.resetFields();
+            // 关闭模态框
+            this.setState({ visible: false });
+            this.reloadData();
+        })
+        
+        });
+    };
+        // 将子组件的引用在父组件中进行保存，方便后期调用
+    saveFormRef = formRef => {
+        this.formRef = formRef;
+    };
+    // 去添加
+    toAdd(){
+        this.setState({comment:{},visible:true})   // 将默认值置空,模态框打开
+    }
+    // 去更新
+    toEdit(record){
+        // 更前先先把要更新的数据设置到state中
+        this.setState({comment:record})
+        // 将record值绑定表单中
+        this.setState({visible:true})
+    }
     render(){
         let columns=[{
             title:'id',
             dataIndex:'id'
         },{
             title:'内容',
-            width:350,
+            width:400,
             dataIndex:'content'
         },{
             title:'评价时间',
-            dataIndex:'comment_time'
+            dataIndex:'commentTime'
         },{
             title:'订单号',
-            dataIndex:'order_id'
+            dataIndex:'orderId'
         },
         {
             title:'操作',
@@ -86,14 +124,13 @@ class CommentPage extends React.Component{
                 return (
                     <div>
                         <Button type ='link' size="small" onClick={this.handleDelete.bind(this,record.id)}>删除</Button>
-                        <Button type ='link' size="small">修改</Button>
+                        <Button type ='link' size="small" onClick={this.toEdit.bind(this,record)}>修改</Button>
                     </div>
                 )
             }
         }]
         const rowSelection = {
             onChange: (selectedRowKeys, selectedRows) => {
-              //console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
               this.setState({
                   ids:selectedRowKeys
               })
@@ -105,10 +142,10 @@ class CommentPage extends React.Component{
           };
 
         return (
-            <div className={styles.customer}>
-                <div className ={styles.title}> 评价管理</div>
+            <div className={styles.comment}>
+                <div className ={styles.title}> 顾客管理</div>
                 <div className ={styles.btns}>
-                    <Button>添加</Button>&nbsp;
+                    <Button onClick={this.toAdd.bind(this)}>添加</Button>&nbsp;
                     <Button onClick={this.handleBatchDelete.bind(this)}>批量删除</Button>&nbsp;
                     <Button type="link">导出</Button>
                 </div>
@@ -120,6 +157,13 @@ class CommentPage extends React.Component{
                 rowSelection={rowSelection}
                 columns={columns}
                 dataSource={this.state.list}/>
+
+                <CommentForm 
+                    initData={this.state.comment}
+                    wrappedComponentRef={this.saveFormRef}
+                    visible={this.state.visible}
+                    onCancel={this.handleCancel}
+                    onCreate={this.handleCreate}/>
             </div>
         )
     }
